3d74d8f327746c237e66574b440f0e499783be6b
anatoliy-a-lobanov <anatoliy.a.lobanov@mail.ru>
2023-07-15 00:19:48 +0300

    progress v8

---
+++ app/src/main/java/ru/lobanov/todoapp/ui/MainComposeActivity.kt
@@ -0,0 +1,35 @@
+package ru.lobanov.todoapp.ui
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainComposeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Greeting("Android")
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+}
